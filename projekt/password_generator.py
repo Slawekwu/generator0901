abbfafdd0710c23b820a0fb19745baff07d4415d
Slawekwu <slaweek71@gmail.com>
2024-01-21 11:08:40 +0100

    todolist

--- projekt/password_generator.py
+++ projekt/password_generator.py
@@ -1,67 +1,75 @@
-import sys
-import random
-import string
+user_choice = -1
 
-password = []
-characters_left = -1
+tasks = []
 
-def update_characters_left(number_of_characters):
-    global characters_left
 
-    if number_of_characters < 0 or number_of_characters > characters_left:
-        print(' liczba znaków spoza przedziału 0,', characters_left)
-        sys.exit(0)
+def show_tasks():
+    task_index = 0
+    for task in tasks:
+        print(task + " [" + str(task_index) + "]")
+        task_index += 1
+
+
+def add_task():
+    task = input("Wpisz treść zadania: ")
+    if task == "0":
+        print("Powrót do menu")
     else:
-        characters_left -= number_of_characters
-        print('pozostało znaków:', characters_left)
-password_lenght = int(input('jak długie ma być hasło? '))
+        tasks.append(task)
+        print("Dodano zadanie!")
 
-if password_lenght < 5:
-    print('hasło musi mieć minmum 5 znaków, spróbuj jeszcze raz')
-    sys.exit(0)
-else:
-    characters_left = password_lenght
-    # print('pozostało znaków:', characters_left)
 
-lowercase_letters = int(input("ile małych liter ma miec hasło? "))
-update_characters_left(lowercase_letters)
-#
-uppercase_letters = int(input("ile dużych liter ma miec hasło? "))
-update_characters_left(uppercase_letters)
+def delete_task():
+    task_index = int(input("Podaj indeks zadania do usunięcia: "))
 
-special_characters = int(input("ile znaków specjalnych ma miec hasło? "))
-update_characters_left(special_characters)
+    if task_index < 0 or task_index > len(tasks) - 1:
+        print("Zadanie o tym indeksie nie istnieje")
+        return
 
-digits = int(input("ile cyfr ma miec hasło? "))
-update_characters_left(digits)
-# This is a sample Python script.
-if characters_left > 0:
-    print('nie wzystkie znaki zostały wyświtlone')
-    lowercase_letters += characters_left
+    tasks.pop(task_index)
+    print("Usunięto zadanie!")
 
-print()
-print('długośc hasła:', password_lenght)
-print('małe litery:', lowercase_letters)
-print("duże litery:", uppercase_letters)
-print('znaki specjalne:', special_characters)
-print('cyfry:', digits)
 
-for i in range(password_lenght):
-    if lowercase_letters > 0:
-       password.append(random.choice(string.ascii_lowercase))
-       lowercase_letters -= 1
-    if uppercase_letters > 0:
-       password.append(random.choice(string.ascii_uppercase))
-       uppercase_letters -= 1
-    if special_characters > 0:
-       password.append(random.choice(string.punctuation))
-       special_characters -= 1
-    if digits > 0:
-       password.append(random.choice(string.digits))
-       digits -= 1
+def save_tasks_to_file():
+    file = open("tasks.txt", "w")
+    for task in tasks:
+        file.write(task+"\n")
+    file.close()
 
-random.shuffle(password)
-print('wygenerowane hasło:', ''.join(password))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and sett
+def load_tasks_from_file():
+    try:
+        file = open("tasks.txt")
+
+        for line in file.readlines():
+            tasks.append(line.strip())
+
+        file.close()
+    except FileNotFoundError:
+        return
+
+
+load_tasks_from_file()
+
+while user_choice != 5:
+    if user_choice == 1:
+        show_tasks()
+
+    if user_choice == 2:
+        add_task()
+
+    if user_choice == 3:
+        delete_task()
+
+    if user_choice == 4:
+        save_tasks_to_file()
+
+    print()
+    print("1. Pokaż zadania")
+    print("2. Dodaj zadanie")
+    print("3. Usuń zadanie")
+    print("4. Zapisz zmiany do pliku")
+    print("5. Wyjdź")
+
+    user_choice = int(input("Wybierz liczbę: "))
+    print()
